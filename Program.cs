65c100d43623600ff9e1faf06bec94039db42730
Otávio Hellmann Wiemes <hwotavio@gmail.com>
2024-11-07 22:57:40 -0300

    Sistema e Abstraindo um Celular com POO em C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
-
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero:"47988888888", modelo:"16proMax", imei:"987523451", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone nokia: ");
+Smartphone nokia = new Nokia(numero:"4799999999", modelo:"2280", imei:"63210765845", memoria:8);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
